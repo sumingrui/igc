fde8c27e92f6d731fb0c01ed53741e31a43427cd
sumingrui <sumingrui@gmail.com>
2019-07-15 09:36:15 +0800

    add command

--- command.sh
+++ command.sh
@@ -4,10 +4,8 @@
 # 普通igc卷积 
 for i in $(seq 1 5)
 do 
-
 python s-igc.py --network igc1 --depth 5 --firstgroup 6 --secondgroup 2 --dataset indian_pines --spatial-size 7 --bf 0 --train-scale 0.3 --batch-size 32 \
 --num-epochs 20 --lr 0.001 --lr-factor 0.333 --lr-callback 1 --lr-byepoch 30 --optimizer adam --dropout 0 --pooling max
-
 done
 
 sleep 5;
@@ -15,10 +13,15 @@ sleep 5;
 # 修改dropout 0.1
 for i in $(seq 1 5)
 do 
-
 python s-igc.py --network igc1 --depth 5 --firstgroup 4 --secondgroup 3 --dataset indian_pines --spatial-size 7 --bf 0 --train-scale 0.3 --batch-size 32 \
 --num-epochs 200 --lr 0.001 --lr-factor 0.333 --lr-callback 1 --lr-byepoch 30 --optimizer adam --dropout 0.1 --pooling max
+done
 
+# 修改dropout 0.2
+for i in $(seq 1 5)
+do 
+python s-igc.py --network igc1 --depth 5 --firstgroup 4 --secondgroup 3 --dataset indian_pines --spatial-size 7 --bf 0 --train-scale 0.3 --batch-size 32 \
+--num-epochs 200 --lr 0.001 --lr-factor 0.333 --lr-callback 1 --lr-byepoch 30 --optimizer adam --dropout 0.2 --pooling max
 done
 
 # 普通plain卷积
